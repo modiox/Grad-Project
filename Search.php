9f5484bc4f563b8b5b5a6acb61fd3056c794ab21
Modi Alyalak <36824388+modiox@users.noreply.github.com>
2022-02-16 23:35:21 +0300

    Update Search.php

--- Search.php
+++ Search.php
@@ -6,20 +6,61 @@ $username = "root";
 $password = "";
 $dbname = "Market";
 
+echo '<head>';
+echo '<meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <link rel="icon" href="images/fevicon.png" type="image/gif" />
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>Smart Mart</title>';
+
+  echo '<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+
+  <!-- font awesome style -->
+  <link href="css/font-awesome.min.css" rel="stylesheet" />
+
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+
+</head>';
+
+echo '<body>';
+
+echo '<header class="header_section">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg custom_nav-container ">
+        <a class="navbar-brand" href="index.html">
+          <span>Smart mart</span>
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class=""> </span>
+        </button>';
+echo '<div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav  ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+            </li>
+            </div> '; 
+ echo '</header>';           
 $con = mysqli_connect($servername,$username,$password, $dbname) or die("Cannot connect to database");
 $query = "SELECT Location FROM items WHERE Name='$search'";
 $sql = mysqli_query($con, $query);
 $result = $con -> query($query);
 print("<br>");
-print("<center> <table border='1' style='color:#2A6881 width: 100%;
-    color: #2A6881;
-    font-family: monospace;
-    font-size:16px;
-    text-align:center;'> <thead> <tr>"); 
-print("<th> Loaction </th></tr> </thead>");
-print("<tbody>");
 
 
+ $img = "campus1.png";
 
 if($sql)
 {
@@ -30,23 +71,27 @@ if($result -> num_rows > 0)
 {  while($row1 =$result -> fetch_assoc())
 
  { 
-       echo"<tr><td>".$row1["Location"]."</td></tr>";
-
+      echo "<center> <h2> Loaction: ".$row1["Location"]."</h2> </center>";
+      echo '<center><img src="'. $img. '"></center>';
 
  }
-    print("</tbody> </table> </center>");
+     
 }
 
 else 
     echo "No match found..";
+     echo'<br>';
+    echo '<img src="'. $img. '">';
 
 } } 
 
 else 
-    echo "zero result";
+    echo "Zero result";
 
 mysqli_close($con);
 
 
 ?>
+
+
 
